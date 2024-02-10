454c9a39b52dfe7f5e12afdcd57b0cc0bf2e6122
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-02-10 16:43:41 -0500

    add function to create a row of squares

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const container = document.querySelector(".container");
+
+function createRow(length) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let i = 0; i < length; i++) {
+        const rowItem = document.createElement("div");
+        rowItem.classList.add("row-item");
+        row.append(rowItem);
+    }
+    container.append(row);
+}
