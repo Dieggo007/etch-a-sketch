d1f13380742bdf6f77ed82f3319e47fe21c457b6
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-02-11 15:42:43 -0500

    add two functions to get and darken color

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function createRow(length) {
 
     for (let i = 0; i < length; i++) {
         const rowItem = document.createElement("div");
+        rowItem.setAttribute("data-filter", 100);
         rowItem.classList.add("row-item");  
         row.append(rowItem);
     }
@@ -19,10 +20,29 @@ function createGrid(length) {
     for (let i = 0; i < length; i++) createRow(length);
 }
 
+const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const darkenElementColor = (target) => {
+    const filter = parseInt(target.getAttribute("data-filter"));
+    target.style.filter = `brightness(${filter}%)`;
+    target.setAttribute("data-filter", filter - 10);
+}
+
 const onMouseover = (e) => {
-    if (e.target.classList.contains("row-item")) {
-        e.target.style.backgroundColor = "black";
+    if (!(e.target.classList.contains("row-item"))) return;
+    if (e.target.classList.contains("colored")) {
+        darkenElementColor(e.target);
+        return;
     }
+    e.target.style.backgroundColor = getRandomColor();
+    e.target.style.borderColor = "transparent"; 
+    e.target.classList.add("colored");
 }
 
 const createNewGrid = () => {
